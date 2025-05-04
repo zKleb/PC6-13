f97ac67db54736fbc0cf11f374c2aeda222a009e
Owner <Owner@DESKTOP-N4A55MI>
2025-05-04 00:10:24 -0500

    Add project files.

---
+++ PC6-13/PC6-13.cpp
@@ -0,0 +1,37 @@
+// Caleb Redeker
+// Beginning Programming
+// Professor Goulden
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int employeeCount();
+int missedDays(int&);
+
+int main() {
+	int numOfEmployees = 0;
+
+	employeeCount();
+	missedDays(numOfEmployees);
+
+}
+
+int employeeCount() {
+	int count;
+
+	cout << "Number of Employees: ";
+	cin >> count;
+
+	while (count < 1) {
+		cout << "Invalid count. Input an integer greater than 0." << endl;
+		cout << endl;
+		cout << "Number of Employees: ";
+		cin >> count;
+	}
+	return count;
+}
+
+
+
