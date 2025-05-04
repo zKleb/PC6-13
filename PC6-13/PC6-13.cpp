3beac070c6d0a4d2b7976762cfd9cea370e91520
Caleb Redeker <caleb.a.redeker@my.occc.edu>
2025-05-04 02:29:02 -0500

    Complete program, acts as intended.

--- PC6-13/PC6-13.cpp
+++ PC6-13/PC6-13.cpp
@@ -9,20 +9,19 @@ using namespace std;
 
 int employeeCount();
 int missedDays(int&);
-double averageDays(int, int);
+double averageDays(int&, int&);
 
 int main() {
-	int count, daysOut = 0;
 	
 
-	employeeCount();
-	missedDays(count);
-	cout << "The average number of days missed by employees is " << averageDays(count, daysOut) << "." << endl;
+	int count = employeeCount();
+	int daysOutTotal = missedDays(count);
+	averageDays(count, daysOutTotal);
 
 }
 
 int employeeCount() {
-	int count;
+	double count;
 
 	cout << "Number of Employees: ";
 	cin >> count;
@@ -37,18 +36,27 @@ int employeeCount() {
 }
 
 int missedDays(int& count) {
-	int daysOut = 0;
+	int daysOut = 0, daysOutTotal = 0;
 	
 	for (int i = 0; i < count; i++) {
-		cout << "Days out for employee " << i << ":" << endl;
+		cout << "Days out for employee " << i+1 << ":" << endl;
 		cin >> daysOut;
+		while (daysOut < 0) {
+			cout << "Invalid input. Input an integer of zero or greater." << endl;
+			cout << "Days out for employee " << i + 1 << ":" << endl;
+			cin >> daysOut;
+		}
+		
+		daysOutTotal += daysOut;
+		
 	}
-
-	return daysOut;
+	return daysOutTotal;
 }
 
-double averageDays(int count, int daysOut) {
-	double theAverage;
-	theAverage = daysOut / count;
+double averageDays(int& count, int& daysOutTotal) {
+	double theAverage = 0;
+	theAverage = static_cast<double>(daysOutTotal) / static_cast<double>(count);
 
+	cout << "The average number of days missed is " << fixed << setprecision(2) << showpoint << theAverage << endl;
+	return theAverage;
 }
